fa305a3efa9d2bf321cd727f80744303ed2e6067
Ktmich2095 <230881@utxicotepec.edu.mx>
2024-10-26 16:53:18 -0600

    Comenzando la página de registro

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,11 +1,13 @@
 
 const formularioLogin = (req, res) =>{
     res.render('auth/login',{
+
     })
 }
 
 const formularioRegistro = (req, res) =>{
     res.render('auth/registro',{
+        pagina:'Crear Cuenta'
     })
 }
 

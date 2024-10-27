dfac6118593185db9d3cc841fe4a24031aa46a80
Ktmich2095 <230881@utxicotepec.edu.mx>
2024-10-26 19:39:47 -0600

    Autollenado de campos validados

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -18,18 +18,19 @@ const registrar = async (req,res) =>{
     await check('nombre').notEmpty().withMessage('El nombre es obligatorio').run(req)
     await check('email').isEmail().withMessage('Eso no parece un Email').run(req)
     await check('password').isLength({min:8}).withMessage('El password debe ser de al menos 8 caracteres').run(req)
-    await check('repetir_password').equals('password').withMessage('Los passwords no son iguales').run(req)
+    await check('repetir_password').equals(req.body.password).withMessage('Los Passwords no son iguales').run(req)
     
     let resultado = validationResult(req) 
-
-
-    
     //Verificar que el resultado este vacio
     if(!resultado.isEmpty()){
     //ERRORES  
         return res.render('auth/registro',{
             pagina:'Crear Cuenta',
-            errores: resultado.array()
+            errores: resultado.array(),
+            usuario:{
+                nombre: req.body.nombre,
+                email: req.body.email
+            }
         })
     }
 

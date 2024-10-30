bc2d4e4a054bc8190fe40b70c025d7d2e5cce40d
Ktmich2095 <230881@utxicotepec.edu.mx>
2024-10-29 21:39:38 -0600

    Obtener latitud y longitud del pin

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -2,11 +2,26 @@
     const lat = 20.67444163271174;
     const lng = -103.38739216304566;
     const mapa = L.map('mapa').setView([lat, lng ], 16);
-    
+    let marker
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapa);
+
+    //PIN
+    marker = new L.marker([lat,lng],{
+        draggable:true,//permite mover el pin
+        autoPan:true//permite mover el mapa de acuerdo al pin
+    })
+    .addTo(mapa)
+
+    //Detectar el movimiento del pin
+    marker.on('moveend',function(e){
+        marker = e.target
+        const posicion=marker.getLatLng()
+        console.log(posicion)
+        mapa.panTo(new L.LatLng(posicion.lat,posicion.lng))
+    })
 
 
 })()

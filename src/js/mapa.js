2f14b1c8539e125fa7994063c8103ba0a39e8818
Ktmich2095 <230881@utxicotepec.edu.mx>
2024-11-03 20:16:09 -0600

    Almacenar la última ubicación del pin

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -1,6 +1,7 @@
 (function() {
-    const lat = 20.67444163271174;
-    const lng = -103.38739216304566;
+    //logical Or
+    const lat = document.querySelector('#lat').value || 20.67444163271174;// ?? se ejecuta cuando el primero es null o undefined
+    const lng = document.querySelector('#lng').value || -103.38739216304566;
     const mapa = L.map('mapa').setView([lat, lng ], 16);
     let marker
 

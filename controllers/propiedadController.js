0e9bb63289f630b00bc8dd87ff6b4ee0e8165350
Ktmich2095 <230881@utxicotepec.edu.mx>
2024-11-06 22:27:48 -0600

    Mostrar mensajes condicionales

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,9 +1,16 @@
 import { validationResult } from "express-validator"
 import {Precio,Categoria,Propiedad} from "../models/index.js"
 
-const admin = (req,res) =>{
+const admin = async(req,res) =>{
+    const {id}  = req.usuario
+    const propiedades = await Propiedad.findAll({
+        where:{
+            usuarioId:id
+        }
+    })
     res.render('propiedades/admin',{
-        pagina:'Mis propiedades'
+        pagina:'Mis propiedades',
+        propiedades,
     })
 }
 

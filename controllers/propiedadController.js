9e77df10639026a14873a668f456b2601c4e4d32
Ktmich2095 <230881@utxicotepec.edu.mx>
2024-11-09 11:48:32 -0600

    Consultar tablas relacionadas con Sequelize

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -6,7 +6,12 @@ const admin = async(req,res) =>{
     const propiedades = await Propiedad.findAll({
         where:{
             usuarioId:id
-        }
+        },
+        include:[
+            {model:Categoria,as:'categoria'},
+            {model:Precio,as:'precio'}
+        ]
+
     })
     res.render('propiedades/admin',{
         pagina:'Mis propiedades',

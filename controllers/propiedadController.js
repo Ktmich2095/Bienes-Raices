c362bd0f62e048142b1094abc87c479798645990
Ktmich2095 <230881@utxicotepec.edu.mx>
2024-11-16 22:53:04 -0600

    Generando los numeros de la paginación

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -20,21 +20,30 @@ const admin = async(req,res) =>{
         const limit= 10;
         const offset=((paginaActual * limit) - limit)
 
-        const propiedades = await Propiedad.findAll({
-            limit,
-            offset,
-            where:{
-                usuarioId:id
-            },
-            include:[
-                {model:Categoria,as:'categoria'},
-                {model:Precio,as:'precio'}
-            ]
-        })
+        const [propiedades,total] = await Promise.all([
+            Propiedad.findAll({
+                limit,
+                offset,
+                where:{
+                    usuarioId:id
+                },
+                include:[
+                    {model:Categoria,as:'categoria'},
+                    {model:Precio,as:'precio'}
+                ]
+            }),
+            Propiedad.count({
+                where:{
+                    usuarioId:id
+                }
+            })
+        ])
+
         res.render('propiedades/admin',{
             pagina:'Mis propiedades',
             propiedades,
             csrfToken:req.csrfToken(),
+            paginas:Math.ceil(total/limit)
         })
     } catch (error) {
         console.log(error)
